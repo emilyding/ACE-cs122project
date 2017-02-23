6520440f55c4a25941c1330b7c86841f43cad9ca
Anne Rogers <amr@cs.uchicago.edu>
2017-02-23 12:37:05 -0600

    help zagat

--- scraping/zagat/z_scrape.py
+++ scraping/zagat/z_scrape.py
@@ -1,28 +1,158 @@
-API_key = "AIzaSyCP58u53oJ_brOYoNkF0ktaCE2EyZaJIyA"
-
 import requests
 import re
-import util
-import bs4
-import queue
+#import bs4
 import json
-import sys
+#import sys
 import csv
-url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyCP58u53oJ_brOYoNkF0ktaCE2EyZaJIyA&callback=?"
+
+test_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.881832,-87.623177&radius=5000&type=restaurant&key="
 #url = "https://www.zagat.com/r/goosefoot-chicago"
-def cook_soup(url):
+
+def run_code():
+    # Camille's API
+    API = "AIzaSyCP58u53oJ_brOYoNkF0ktaCE2EyZaJIyA"
+    
+    # Emily's API
+    # API = "AIzaSyDW15a3LCSe7J1YDRvUMWR1IsVlMxqQtRU"
+    
+    # Austin's API
+    # API = "AIzaSyB28WD_QVBYhUlO3fr22uMNr2zemUA7ZyQ"
+    run_number = 5 #haven't done this one yet
+    full_results = []
+
+    # locations dict with key city: [lat, long, radius (est.)]
+    locations = {"Chicago": [41.881832, -87.623177, 15.297]}#,
+    #            "LA": [34.052235, -118.243683, 22.428],
+    #            "New York": [40.730610, -73.935242, 17.453],
+    #            "San Francisco": [37.733795, -122.446747, 6.846],
+    #            "Houston": [29.7604, -95.3698, 25.040]}
+    for city in locations:
+        city_name = city
+        info = locations[city]
+
+        # Converts radius from miles into lat/lon degrees
+        radius_deg = info[2] / 69
+        
+        #lat = info[0] - radius_deg # COMMENT OUT IF MID CITY
+        lat = 42.040136347826036# UNCOMMENT IF MID CITY
+        #lon = info[1] - radius_deg # COMMENT OUT IF MID CITY
+        lon = -87.69487265217385# UNCOMMENT IF MID CITY
+        start_lon = info[1] - radius_deg
+        stop_lat = info[0] + radius_deg
+        stop_lon = info[1] + radius_deg
+
+        data, lat, lon = cook_soup(
+            API, lat, lon, stop_lat, stop_lon, start_lon)
+        full_results = full_results + data
+
+    print(city_name)
+    print("lat:", lat)
+    print("lon:", lon)
+
+    csv_name = "zagat_" + city_name + "_" + str(run_number) + ".csv"
+    with open(csv_name,"w") as f:
+        wr = csv.writer(f)
+        wr.writerows([["name", "place_id", "price_level", "rating"]] + full_results)
+
+    return (city_name, lat, lon)
+
+def cook_soup(API, lat, lon, stop_lat, stop_lon, start_lon):
+    url_1 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
+    url_2 = "&radius=1000&type=restaurant&key="
+    
+    results = []
+    request_num = 0
+        
+    while request_num < 500: #- 3:
+        # Loops over city area, pulls restaurant results
+        if lat >= stop_lat and lon >= stop_lon:
+            print("finished searching city")
+            break
+
+        if lat <= stop_lat:            
+            if lon < stop_lon:
+                loc_url = "location=" + str(lat) + "," + str(lon)
+                url = url_1 + loc_url + url_2 + API
+                request_dict = get_results(url)
+                list_results = request_dict["results"]
+                for result in list_results:
+                    name = result.get("name", None)
+                    place_id = result.get("place_id", None)
+                    price_level = result.get("price_level", None)
+                    rating = result.get("rating", None)
+                    results.append([name, place_id, price_level, rating])
+            
+                # If > 20 locations, pulls up to 40 more
+                if "next_page_token" in request_dict:
+                    request_dict = get_results(
+                        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" 
+                        + request_dict["next_page_token"] + "&key=" + API)
+                    list_results = request_dict["results"]
+                    for result in list_results:
+                        name = result.get("name", None)
+                        place_id = result.get("place_id", None)
+                        price_level = result.get("price_level", None)
+                        rating = result.get("rating", None)
+                        results.append([name, place_id, price_level, rating])
+                    request_num += 1
+                    if "next_page_token" in request_dict:
+                        request_dict = get_results(
+                            "https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken=" 
+                            + request_dict["next_page_token"] + "&key=" + API)
+                        list_results = request_dict["results"]
+                        for result in list_results:
+                            name = result.get("name", None)
+                            place_id = result.get("place_id", None)
+                            price_level = result.get("price_level", None)
+                            rating = result.get("rating", None)
+                            results.append([name, place_id, price_level, rating])
+                        request_num += 1
+                lon += .01
+                request_num += 1
+                print(lat, lon)
+
+            else:
+                lon = start_lon
+                lat += .01
+
+    # Gets rid of duplicates
+    results = list(uniq(sorted(results, reverse=True)))
+
+    return results, lat, lon
+
+def uniq(lst):
+    last = object()
+    for item in lst:
+        if item == last:
+            continue
+        yield item
+        last = item
+
+def get_results(url):
+    '''
+    Input:
+        - url
+    Output:
+        - list of result restaurants
+    '''
+    # Stores page results in variable data. Dictionary with key "results"
+    # in the form of list of dictionaries of each result. Relevant keys:
+    # name, rating, place_id, next_page_token
+
     request = requests.get(url)
     data = request.json()
     with open('data.json', 'w') as f:
         json.dump(data, f)
-    #data now stored in variable data
+    return data
+
+
 
 #    encoding = request.encoding
 #    html = request.text.encode(encoding)
 #    soup = bs4.BeautifulSoup(html, "html5lib")
 
 #    return soup
-
+'''
 
 def get_info(soup):
     name_tag = soup.find_all('h1', id = "main-content-title")
@@ -87,3 +217,4 @@ def get_info(soup):
 #    word_list = []
 
 #    words = re.findall("[A-Za-z]\w*", lower_text)
+'''

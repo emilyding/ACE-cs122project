6e6092d4ed03c2e1945b55e81bcfb2be6e78c2e6
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 15:28:51 -0600

    dollar signs

--- graphs.py
+++ graphs.py
@@ -19,19 +19,18 @@ def gen_piechart(query):
     plt.show()
     '''
     price_rate = yelp_sql.price_ratings(query)
-    labels = ['$', '\$$', '\$\$\$', '\$\$\$\$']
     prices = ["$", "$$", "$$$", "$$$$"]
     sizes = []
+    labels = []
     for dollar_signs in prices:
         sizes.append(price_rate[dollar_signs][1])
-    print(sizes)
-    labels = labels[:len(sizes)]
+    for i in  range(len(sizes)):
+        labels.append((i+1)*"\$")
     print(labels)
     colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
  
     # Plot
-    plt.pie(sizes, labels=labels, colors=colors,
-        autopct='%1.1f%%', startangle=0)
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=0)
  
     plt.axis('equal')
     plt.show()
